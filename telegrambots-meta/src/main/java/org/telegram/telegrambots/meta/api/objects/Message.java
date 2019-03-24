9d14713dad4257bdcad9c9b4564735e4df397dc7
iBranch211 <iBranch211@outlook.com>
2019-03-24 20:17:48 +0100

    Add TelegramFileDownloader

--- telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/Message.java
+++ telegrambots-meta/src/main/java/org/telegram/telegrambots/meta/api/objects/Message.java
@@ -381,14 +381,6 @@ public class Message implements BotApiObject {
         return this.video != null;
     }
 
-    public boolean hasAudio(){
-        return this.audio != null;
-    }
-
-    public boolean hasVoice(){
-        return this.voice != null;
-    }
-
     public boolean isReply() {
         return this.replyToMessage != null;
     }
